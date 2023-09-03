fc86b0154cf00506f53c309591a2964a315023fc
Milon <plmilonpl@gmail.com>
2023-09-03 16:34:31 +0200

    Add ping.js

---
+++ commands/utility/ping.js
@@ -0,0 +1,11 @@
+﻿const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Check the bot status'),
+	async execute(interaction) {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		await interaction.editReply(`:ping_pong: Pong!\n:stopwatch: Uptime: ${Math.round(interaction.client.uptime / 60000)} minutes\n:round_pushpin: Rountrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+};
