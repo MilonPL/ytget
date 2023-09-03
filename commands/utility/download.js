d287870ad2b82562a0d0679a0930d6ff16efe9be
Milon <plmilonpl@gmail.com>
2023-09-03 21:31:43 +0200

    Add embeds

--- commands/utility/download.js
+++ commands/utility/download.js
@@ -1,7 +1,8 @@
 // Require the necessary modules
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const youtubedl = require('youtube-dl-exec');
 const crypto = require('crypto');
+const fs = require('fs');
 
 // Azure Storage dependency
 const {
@@ -28,51 +29,87 @@ const containerName = `downloaded`;
 
 // Main command
 module.exports = {
- data: new SlashCommandBuilder()
-    .setName('download')
-    .setDescription('Download a YouTube video!')
-    .addStringOption(option =>
-      option
-        .setName('link')
-        .setDescription('Link to the video you want to download')
-        .setRequired(true)),
-  async execute(interaction) {
-    try {
-      const id = crypto.randomBytes(5).toString('hex');
-      const link = interaction.options.getString('link'); // Get the link from the user
-        await interaction.reply(`Downloading the video! ${id}`)
+	data: new SlashCommandBuilder()
+		.setName('download')
+		.setDescription('Download a YouTube video!')
+		.addStringOption(option =>
+			option
+				.setName('link')
+				.setDescription('Link to the video you want to download')
+				.setRequired(true))
+		.addStringOption(option =>
+			option
+				.setName('format')
+				.setDescription('Select the file format')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'mp4', value: 'mp4' },
+					{ name: 'm4a', value: 'm4a' },
+					{ name: '3gp', value: '3gp' },
+					{ name: 'webm', value: 'webm' }
+				)
+		),
+	async execute(interaction) {
+		try {
+			const id = crypto.randomBytes(5).toString('hex');
+			const link = interaction.options.getString('link'); // Get the link from the user
+			const format = interaction.options.getString('format');
+			// Downloading embed here
+			const embedDownloading = new EmbedBuilder()
+				.setColor(0x0099FF)
+				.setTitle(':computer: Downloading the video!')
+				.setDescription(':hourglass_flowing_sand: Please wait...')
+				.addFields(
+					{ name: ':bulb: Link:', value: `${link}` },
+					{ name: ':movie_camera: File format:', value: `${format}` }
+			)
+				.setTimestamp()
+				.setFooter({ text: 'This can take several minutes, depending on the video size.' })
+			await interaction.reply({ embeds: [embedDownloading] });
 
-      // Download the YouTube video with youtube-dl-exec and wrap it in a Promise
-      const downloadPromise = new Promise((resolve, reject) => {
-        youtubedl(link, {
-          restrictFilenames: true,
-          output: id + '.%(ext)s',
-          noPart: true,
-        })
-          .then(info => {
-            const localFilePath = id + '.mp4';
-            resolve(localFilePath); // Resolve the Promise with the local file path
-          })
-          .catch(error => {
-            reject(error); // Reject the Promise if there's an error
-          });
-      });
-
-      const localFilePath = await downloadPromise; // Wait for the download to finish
-
-      const blobName = id + '.mp4';
-      const blockBlobClient = new BlockBlobClient(
-        `${baseUrl}/${containerName}/${blobName}`,
-        sharedKeyCredential
-      );
-
-      await blockBlobClient.uploadFile(localFilePath);
-
-      console.log(`Blob ${blockBlobClient.url} created`);
-      return interaction.editReply(`Downloaded the video! Access it at ${baseUrl}/${containerName}/${blobName}`);
-    } catch (error) {
-      console.error('Error:', error);
-      return interaction.editReply('An error occurred while downloading the video.');
-    }
-  },
+			// Download the YouTube video with youtube-dl-exec and wrap it in a Promise
+			const downloadPromise = new Promise((resolve, reject) => {
+				youtubedl(link, {
+					restrictFilenames: true,
+					output: id + '.%(ext)s',
+					noPart: true,
+					noPlaylist: true,
+					maxFilesize: '250M',
+					ignoreErrors: true,
+					format: format
+				})
+					.then(info => {
+						const localFilePath = `${id}.${format}`;
+						resolve(localFilePath); // Resolve the Promise with the local file path
+					})
+					.catch(error => {
+						reject(error); // Reject the Promise if there's an error
+					});
+			});
+			const localFilePath = await downloadPromise; // Wait for the download to finish
+			const blobName = `${id}.${format}`;
+			const blockBlobClient = new BlockBlobClient( // Create the blob
+			`${baseUrl}/${containerName}/${blobName}`,
+			 sharedKeyCredential
+			);
+			await blockBlobClient.uploadFile(localFilePath); // Send data to blob
+			console.log(`Blob ${blockBlobClient.url} created`);
+			fs.unlinkSync(`${id}.${format}`)
+			// Success embed here
+			const embedDownloaded = new EmbedBuilder()
+				.setColor(0x37D600)
+				.setTitle(':white_check_mark: Successfully downloaded the video!')
+				.setDescription(`You can access it at https://ytget.ogdg.pl/${containerName}/${blobName}`)
+				.setTimestamp()
+				.setFooter({ text: 'Thank you for using ytget!' })
+			return interaction.editReply({ embeds: [embedDownloaded] }); // Update the reply
+		} catch (error) {
+			console.error('Error:', error);
+			// Error embed here
+			const embedError = new EmbedBuilder()
+				.setColor(0xFF1F00)
+				.setTitle(':x: An error occurred while downloading the video.')
+			return interaction.editReply({ embeds: [embedError] });
+		}
+	},
 };
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,22 @@
-﻿const { SlashCommandBuilder } = require('discord.js');
+﻿const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Check the bot status'),
 	async execute(interaction) {
-		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-		await interaction.editReply(`:ping_pong: Pong!\n:stopwatch: Uptime: ${Math.round(interaction.client.uptime / 60000)} minutes\n:round_pushpin: Rountrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		const embedPinging = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle('Pinging...')
+		const sent = await interaction.reply({ embeds: [embedPinging], fetchReply: true });
+		const embedPinged = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle(':ping_pong: Pong!')
+			.addFields(
+				{ name: ':stopwatch: Uptime:', value: `${Math.round(interaction.client.uptime / 60000)} minutes` },
+				{ name: ':round_pushpin: Latency:', value: `${sent.createdTimestamp - interaction.createdTimestamp}ms`})
+			.setTimestamp()
+			.setFooter({ text: 'ytget', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+		await interaction.editReply({ embeds: [embedPinged] });
 	},
 };
